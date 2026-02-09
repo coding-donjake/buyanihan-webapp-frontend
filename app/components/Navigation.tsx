bcacc4d55afc9d0393b8ae50ed6e00fe76312ac2
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2026-02-09 14:08:57 +0800

    update

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,84 @@
+"use client";
+// import banner from "./assets/banner.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBell, faMagnifyingGlass, faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useRouter, usePathname } from "next/navigation";
+import { useState } from "react";
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <div className="">
+      <div className="flex flex-row items-center border-b border-[#450C00]">
+        <img className="w-auto h-16" src="/assets/banner.jpg" alt="buyanihan" />
+        <span className="ml-auto text-4xl text-[#006723] cursor-pointer sm:hidden" onClick={() => setIsOpen(true)}>
+          <FontAwesomeIcon icon={faBars} />
+        </span>
+        <div className="hidden sm:flex flex-row ml-auto">
+          <div className="flex flex-row gap-2 items-center px-4 rounded-full border mr-6 border-[#006723]">
+            <input type="text" />
+            <span className="cursor-pointer">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </span>
+          </div>
+          <span className="ml-auto text-4xl text-[#006723] cursor-pointer">
+            <FontAwesomeIcon icon={faBell} />
+          </span>
+          <span className="ml-auto text-4xl text-[#006723] cursor-pointer">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </span>
+          <span className="ml-auto text-4xl text-[#006723] cursor-pointer">
+            <FontAwesomeIcon icon={faUser} />
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-row pt-2">
+        <button
+          className={`p-2 ${pathname == "/" ? "bg-[#006723] font-bold text-white" : ""} hover:underline`}
+          onClick={() => router.push("/")}
+        >
+          Home
+        </button>
+        <button
+          className={`p-2 ${pathname == "/products" ? "bg-[#006723] font-bold text-white" : ""} hover:underline`}
+          onClick={() => router.push("/products")}
+        >
+          All Products
+        </button>
+        <button
+          className={`p-2 ${pathname == "/contact-us" ? "bg-[#006723] font-bold text-white" : ""} hover:underline`}
+          onClick={() => router.push("/contact-us")}
+        >
+          Contact Us
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`${isOpen ? "absolute" : "hidden"} absolute top-0 left-0 z-10 w-screen max-w-screen h-screen max-h-screen bg-black/80`}
+        onClick={() => setIsOpen(false)}
+      >
+        <div className="flex flex-col gap-4 w-64 h-screen ml-auto p-4 bg-white" onClick={(e) => e.stopPropagation()}>
+          <button className={`p-2 ${pathname == "/" ? "bg-[#006723] font-bold text-white" : ""} hover:underline`}>
+            Cart
+          </button>
+          <button
+            className={`p-2 ${pathname == "/products" ? "bg-[#006723] font-bold text-white" : ""} hover:underline`}
+          >
+            Navigations
+          </button>
+          <button
+            className={`p-2 ${pathname == "/contact-us" ? "bg-[#006723] font-bold text-white" : ""} hover:underline`}
+          >
+            Profile
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,48 @@
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Navigation from "./components/Navigation";
+// import product from "/assets/product.webp";
+
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-4 px-10 max-w-5xl mx-auto">
+      <Navigation />
+      <div className="mt-8">
+        <div className="font-bold text-3xl text-[#006723]">Buy Agricultural Products Online</div>
+        <div className="mt-6 font-medium text-xl text-[#006723]">Top Selling Products</div>
+        <div className="flex flex-row gap-2 items-center mt-2">
+          <div className="flex flex-row flex-wrap gap-4 justify-center w-full p-6 rounded-md bg-[#F5EFDD]">
+            <div className="w-52 h-52 border border-gray-100 rounded shadow-lg cursor-pointer bg-white">
+              <img className="w-full h-full object-cover" src="/assets/product.webp" alt="" />
+            </div>
+            <div className="hidden sm:block w-52 h-52 border border-gray-100 rounded shadow-lg cursor-pointer bg-white">
+              <img className="w-full h-full object-cover" src="/assets/product.webp" alt="" />
+            </div>
+            <div className="hidden lg:block w-52 h-52 border border-gray-100 rounded shadow-lg cursor-pointer bg-white">
+              <img className="w-full h-full object-cover" src="/assets/product.webp" alt="" />
+            </div>
+            <div className="hidden lg:block w-52 h-52 border border-gray-100 rounded shadow-lg cursor-pointer bg-white">
+              <img className="w-full h-full object-cover" src="/assets/product.webp" alt="" />
+            </div>
+          </div>
+          <div className="absolute left-1/2 w-screen -translate-x-1/2 max-w-5xl">
+            <div className="flex flex-row justify-between">
+              <span className="text-4xl text-[#006723] cursor-pointer">
+                <FontAwesomeIcon icon={faChevronLeft} />
+              </span>
+              <span className="text-4xl text-[#006723] cursor-pointer">
+                <FontAwesomeIcon icon={faChevronRight} />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 items-center my-8">
+          <div className="text-xl italic text-[#006723]">Interested in selling products?</div>
+          <button className="p-2 bg-[#B68424] text-white hover:underline">Click to Know More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
